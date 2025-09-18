eda81a262b13d54321c0f09ffbd1e19a4ddb0473
xsalles <xs.salles@gmail.com>
2025-09-18 20:35:40 -0300

    feat: added the fonts in the project

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -3,6 +3,7 @@ import { Button } from "../button/button";
 
 @Component({
   selector: 'component-header',
+  standalone: true,
   imports: [Button],
   templateUrl: './header.html',
   styleUrl: './header.scss'
---
+++ src/app/template/hero-section/hero-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-section',
+  imports: [],
+  templateUrl: './hero-section.html',
+  styleUrl: './hero-section.scss'
+})
+export class HeroSection {
+
+}
