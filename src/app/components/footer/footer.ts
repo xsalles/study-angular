5288b18c3e07a1af07877a7a3cd11a6f581b7b78
xsalles <xs.salles@gmail.com>
2025-09-22 17:04:20 -0300

    feat: creted the structure of html in the footer

---
+++ src/app/components/footer/footer.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PrimaryButton } from "../button/primary-button/primary-button";
+
+@Component({
+  selector: 'component-footer',
+  imports: [PrimaryButton],
+  templateUrl: './footer.html',
+  styleUrl: './footer.scss'
+})
+export class Footer {
+
+}
